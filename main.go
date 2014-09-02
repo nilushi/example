3447408dbc50d34e2ffa94b603b3a07d33c608da
nilushi <chathuriseneviratne@Chathuris-iMac.local>
2014-09-02 17:32:55 +0530

    updating example file for solr

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 
 func main() {
 
-	s, err := solr.NewSolr(`http://192.168.1.100:8983/solr`, `wojka`, 2)
+	s, err := solr.NewSolr(`http://192.168.1.100:8983/solr`, `wojka`)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	addDocuments(s)
+	FetchSuggestions(s, `baby`)
 	//	addDocuments(s)
 
 	fmt.Println(`================================== Fetch by Distance =====================================`)
@@ -30,13 +33,13 @@ func main() {
 
 	fmt.Println(`================================== Fetch by Relevance and Distance  =====================================`)
 
-	result, err = FetchByRelevanceAndDistance(s, `trainer`, 7.3, 79.5, []string{`SIDEJOB`}, 0, 100, 100000)
+	result, err = FetchByRelevanceAndDistance(s, `car`, 7.3, 79.5, []string{`SIDEJOB`}, 0, 100, 100000)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for i, v := range result.Documents {
-		fmt.Printf("result[%v] : Title (%v), Skills (%v)\n", i, v[`keywords`], v[`skills`])
+		fmt.Printf("result[%v] : Title (%v), Skills (%v), id (%v)\n", i, v[`keywords`], v[`skills`], v[`id`])
 	}
 
 	fmt.Println(`================================== Fetch Sample Records by Relevance =====================================`)
@@ -47,8 +50,17 @@ func main() {
 	}
 
 	for i, v := range result.Documents {
-		fmt.Printf("result[%v] : Title (%v), Skills (%v)\n", i, v[`keywords`], v[`skills`])
+		fmt.Printf("result[%v] : Title (%v), Skills (%v), id (%v)\n", i, v[`keywords`], v[`skills`], v[`id`])
 	}
+
+}
+
+func FetchSuggestions(s *solr.Solr, keyword string) {
+
+	query := solr.NewSuggestQuery(keyword)
+	res, _ := s.Suggest(query)
+
+	fmt.Println(res.Suggestions)
 
 }
 
@@ -98,13 +110,24 @@ func FetchByRelevanceAndDistance(s *solr.Solr, keywords string, lat float64, lon
 	query.SpacialParam(lat, long, `location`, maxDistance)
 	query.Sort(`score desc`)
 	query.BoostFunction(solr.Recip(`geodist()`, 1, 1, 1, 4))
+	query.AddHighlighter(`skills`, 2)
+
 	result, err = s.Search(query)
+	for _, val := range result.Highlights {
+		fmt.Println(val.Id)
+		if terms, ok := val.HighlightTerms[`skills`].([]interface{}); ok {
+			for _, v := range terms {
+				fmt.Println(v)
+			}
+		}
+
+	}
 	return
 
 }
 
 func addDocuments(s *solr.Solr) {
-	doc1 := map[string]interface{}{`id`: `doc3`, `keywords`: `baby sitter`, `location`: `43.697225, -79.404949`, `lables`: []string{`SIDEJOB`}, `skills`: []string{`nanny`, `baby sitter`}, `accounttype`: `EXAMPLE`}
+	doc1 := map[string]interface{}{`id`: `doc3`, `keywords`: `baby sitter`, `location`: `43.697225, -79.404949`, `labels`: []string{`SIDEJOB`}, `skills`: []string{`nanny`}, `accounttype`: `EXAMPLE`}
 	err := s.AddDocuments(doc1)
 	fmt.Println(err)
 
